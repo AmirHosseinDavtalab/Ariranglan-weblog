9a461237fb7d5a08b87a519527441a476df34ce7
jisoorose <ahd.iu2002@gmail.com>
2024-07-25 22:58:52 +0330

    update code

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace="blog"))
+    path('blog/', include('blog.urls', namespace="blog")),
+    path('', lambda request: HttpResponseRedirect('/blog/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
